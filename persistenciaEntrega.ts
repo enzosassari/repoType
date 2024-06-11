f0ae707fe35ce5f3ef2f97d473364e5b0edb83fb
enzosassari <enzosassari@hotmail.com>
2024-06-10 21:21:13 -0300

    Agrego funciones...

--- persistenciaEntrega.ts
+++ persistenciaEntrega.ts
@@ -5,7 +5,6 @@ en un archivo “precios.txt” y “productos.txt” respectivamente.
 Luego recupera la información en forma de array nuevamente y mostrala por consola.*/
 
 import * as fs from 'node:fs';
-//import fs from 'node:fs' ;
 
 const precios: number[] = [525, 3500, 400, 1999];
 const productos: string[] = ["Leche", "Galletitas", "Harina", "Queso", "Verduras"];
@@ -17,27 +16,15 @@ function cargarTxt (arrayCarga : string[] | number[]) : string{
     let contenidoArray : string = "";
 
     for (i=0; i<arrayCarga.length; i++ ){       
-        contenidoArray += arrayCarga[i]+" ";         //guardo los precios y productos en una variable string 
+        contenidoArray += arrayCarga[i]+" "; //guardo los valores del arreglo en una variable string 
     }
 
-    return contenidoArray;                             //retorno la variable string con la cadena
+    return contenidoArray; //retorno la variable string con los valores del arreglo
 }
 
-/**let i : number ;
-let contenidoPrecios : string = ""; 
-let contenidoProductos : string = ""; 
 
-for (i=0; i<precios.length; i++){ //guardo los precios en una variable string 
-    contenidoPrecios += precios[i]+" "; 
-}
-
-for(i=0; i<productos.length;i++){  //guardo los productos en una variable string 
-   contenidoProductos += productos[i]+" "; 
-}
-**/
-
-fs.writeFileSync ('./precios.txt', cargarTxt(precios)); // guardo precios usando la funcion 
-fs.writeFileSync ('./productos.txt', cargarTxt(productos)); //guardo productos usando la funcion 
+fs.writeFileSync ('./precios.txt', cargarTxt(precios)); // guardo precios en txt usando la funcion 
+fs.writeFileSync ('./productos.txt', cargarTxt(productos)); //guardo productos en txt usando la funcion 
 
 //---------------FUNCION PARA LEER LOS TXT----------------//
 function leerTxt (blocNotas : string) : string[] {   
